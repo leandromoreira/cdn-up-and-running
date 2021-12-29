31d356b227aab010bbb9f8bcbee01ad9a0b3d536
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2021-12-29 14:58:39 -0300

    model load test for long tail

--- src/load_tests.lua
+++ src/load_tests.lua
@@ -1,9 +1,21 @@
 math.randomseed(os.time())
 local random = math.random
 
-request = function()
-  local item = "item_" .. random(1, 100)
+local popular_percentage = 96
+local popular_items_quantity = 5
+local max_total_items = 200
 
-  return wrk.format(nil, "/" .. item .. ".ext")
+-- trying to model the long tail
+request = function()
+  local is_popular = random(1, 100) <= popular_percentage
+  local item = ""
+
+  if is_popular then
+    item = "item-" .. random(1, popular_items_quantity)
+  else
+    item = "item-" .. random(popular_items_quantity + 1, popular_items_quantity + max_total_items)
+  end
+
+  return wrk.format(nil, "/path/" .. item .. ".ext")
 end
 
